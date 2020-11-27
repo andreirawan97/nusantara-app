837956441d0cd69e4435ea559e5013a34b846c1b
crevasse02 <wiboworaffi82@gmail.com>
2020-11-28 01:49:36 +0700

    EditProfileNew

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { EditProfileService } from '../service/edit-profile.service';
+// import { EditProfileService } from '../service/edit-profile.service';
 import { Storage } from '@ionic/storage';
 import { RegisterService } from '../service/register.service';
+import { Md5 } from 'md5-typescript';
 
 @Component({
   selector: 'app-tab3',
@@ -11,32 +12,53 @@ import { RegisterService } from '../service/register.service';
 })
 export class Tab3Page {
   username: string;
+  fullname : string;
+  userId: string;
+  // @Input() usernameText: string;
+  @Input() passwordText: string;
+  @Input() nameText: string;
+  errorText: string = "";
   
   constructor(
+
     public storage : Storage, 
     public registerSrv: RegisterService, 
     private route : Router) 
-    {
-      storage.get('user').then((parameter) => {
-        this.showNama(parameter);
-      });
-  }
+    {}
 
   ngOnInit() {}
 
-  showNama(user : any){
-    const data = {
-      username : this.username,
-    };
-    console.log("cek");
-    console.log(data);
-    this.registerSrv.ceklogin(user).subscribe(
-      res=> {
-        console.log("cek1");
+  ionViewWillEnter(){
+    // this.nameText = "lol"; 
+    this.storage.get('nama').then((parameter) => {
+      this.nameText = parameter;
+      // console.log(parameter);
+    })
+    this.storage.get('username').then((parameter) => {
+      this.username = parameter;
+      // console.log(parameter);
+    })
+    this.passwordText = "";
+  }
+
+  simpanProfile(){
+    if (!this.nameText) {
+      this.errorText = "Mohon isi semua field";
+      return;
+    }
+    else{
+      const user ={
+        image    : "test",
+        nama     : this.nameText,
+        username : this.username,
+        newPassword : Md5.init(this.passwordText),
+      };
+      console.log(user);
+      this.registerSrv.editProfile(user).subscribe(res=>{
         console.log(res);
-        this.storage.set('username', this.username);
-      }
-    )
+      })
+    }
+    
   }
   
   toLogout(){
